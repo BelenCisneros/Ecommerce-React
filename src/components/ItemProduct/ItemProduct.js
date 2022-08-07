e0bca7baedf028be60bc8c37de789957be476ed0
Belen Cisneros <dg.cisneros94@gmail.com>
2022-08-07 14:33:17 -0300

    Agregando el contador de producto

--- src/components/ItemProduct/ItemProduct.js
+++ src/components/ItemProduct/ItemProduct.js
@@ -1,13 +1,17 @@
 import './ItemProduct.css'
-
+import ItemCount from '../ItemCount/ItemCount'
 const ItemProduct = ({title, price, image}) => {
 
+    const onAdd = (qty) => {
+        alert(`Agregaste ${qty} productos`);
+    };
+   
     return(
         <div className="item-product">
             <img src={`/assets/${image}`} alt="Imagen producto" />
             <p>{title}</p>
             <span>${price}</span>
-            <button>Comprar</button>
+            <ItemCount onAdd={onAdd} initial={1} stock={7} />
         </div>
     )
 }
